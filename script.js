61654918a29242532679e41959103dd8d1fa6fe6
Milan Dembinski <milandembinski@091.206.028.145.hva.nl>
2014-05-26 09:55:11 +0200

    -

---
+++ script.js
@@ -0,0 +1,68 @@
+// maak namespace aan
+var CAMERAALERT = CAMERAALERT || {};                                    
+
+(function () {
+  CAMERAALERT.controller = {   //controller = object                                     
+
+      init: function () {   //init = methode                                          // Cordova connect met device
+        $$(document).on('deviceready', CAMERAALERT.controller.startObjects);
+      },
+ // kijken of device ready is
+      startObjects: function() {
+        CAMERAALERT.camera.onDeviceReady();                            
+      }
+ 
+  };
+
+  CAMERAALERT.camera = {
+// namespance   object
+    onDeviceReady: function(){ //methode
+        pictureSource=navigator.camera.PictureSourceType; //opent camera
+        destinationType=navigator.camera.DestinationType;   
+    },
+      // camera maakt foto
+    onPhotoDataSuccess: function(imageData){
+        var smallImage = document.getElementById('smallImage');                
+        smallImage.style.display = 'block';
+        smallImage.src = "data:image/jpeg;base64," + imageData;
+    // geeft melding
+        alert('Mooie foto!');                                                   
+    },
+  // als foto succesvol gemaakt is
+    onPhotoURISuccess: function(imageURI){  
+    // zoekt foto
+        var largeImage = document.getElementById('largeImage'); 
+        // laat foto zien
+      largeImage.style.display = 'block'; 
+        // locatie van foto
+      largeImage.src = imageURI;                                            
+    },
+    capturePhoto: function(){
+        navigator.camera.getPicture(CAMERAALERT.camera.onPhotoDataSuccess, CAMERAALERT.camera.onFail, { quality: 50,                            // maakt foto dmv camera
+            destinationType: destinationType.DATA_URL });                                                                                                                                             
+    },
+      // maakt foto en geeft mogelijkheid tot edit
+    capturePhotoEdit: function(){
+    navigator.camera.getPicture(CAMERAALERT.camera.onPhotoDataSuccess, CAMERAALERT.camera.onFail, { quality: 20, allowEdit: true,
+            destinationType: destinationType.DATA_URL });                                                           
+    },
+        // haalt foto van gespecificeerde source
+    getPhoto: function(source){                           
+      navigator.camera.getPicture(CAMERAALERT.camera.onPhotoURISuccess, CAMERAALERT.camera.onFail, { quality: 50,    
+            destinationType: destinationType.FILE_URI,
+            sourceType: source });
+    },
+      // geeft foutmelding
+    onFail: function(message){  
+      alert('Failed because: ' + message);
+    }
+
+
+}
+    //HTML (DOM boom) is ready
+domready(function () {     
+    // Applicatie kan worden gestart
+  CAMERAALERT.controller.init();
+});
+
+  })();
